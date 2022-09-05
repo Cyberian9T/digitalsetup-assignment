749d98240a735e7df3d2eb473a05a43405bb0c75
surakrai <surakraisam@gmail.com>
2022-09-05 15:20:23 +0700

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,48 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import { navLinks } from "../data";
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="sticky-top p-2 overflow-hidden" style={{ background: '#ffffff'}}>
-            <div className="row">
-                <div className="col-2">
-                    <Image src="/Layer_1.png" alt="Logo" width={147} height={53} />
-                </div>
-                <div className="col d-flex justify-content-center">
-                    {navLinks.map((link, index) => {
-                        return (
-                            <div key={index} className="d-flex justify-content-evenly m-3 fs-16">
-                                <Link  href={link.path} >
-                                    <a>{link.name}</a>
-                                </Link>
-                            </div>
-                        );
-                    })}
-                </div>
-                <div className="col-2 z-3">
-                    <div className="dropdown d-flex flex-row-reverse">
-                        <button
-                            className="btn btn-primary text-light dropdown-toggle rounded-pill"
-                            type="button"
-                            data-bs-toggle="dropdown"
-                            id="dropdownMenu"
-                            aria-expanded="false"
-                        >
-                            En
-                        </button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" href="#en">English</a>
-                            <a className="dropdown-item" href="#th">Thai</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <header className="sticky-top py-2 px-4 overflow-hidden bg-white">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-2">
+          <Image src="/Layer_1.png" alt="Logo" width={147} height={53} />
+        </div>
+        <ul className="col-8 list-unstyled d-flex gap-5 justify-content-center mb-0">
+          {navLinks.map((link, index) => {
+            return (
+              <li key={index}>
+                <Link href={link.path}>
+                  <a>{link.name}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="col-2 z-3">
+          <div className="dropdown d-flex flex-row-reverse">
+            <button
+              className="btn btn-primary text-light dropdown-toggle rounded-pill"
+              type="button"
+              data-bs-toggle="dropdown"
+              id="dropdownMenu"
+              aria-expanded="false"
+            >
+              En
+            </button>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#en">
+                English
+              </a>
+              <a className="dropdown-item" href="#th">
+                Thai
+              </a>
             </div>
-        </header>
-    );
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,70 @@
 export default function Projects() {
-    return (
-        <section id='projects' className="p-5 overflow-hidden">
-            <div className="container mt-5 mb-5">
-                <div className="row m-3">
-                    <p className="text-center fs-60">our <span className='text-primary'>projects</span></p>
-                </div>
-                <div className="row d-flex justify-content-center">
-                    <div className="cardshadow col-3 m-3">
-                        <img src="/Sansiri.png" className="card-img-top" alt="..."/>
-                        <div className="card-body p-3">
-                            <p className="card-title fs-26">Sansiri</p>
-                            <p className="card-subtitle fs-14 mb-2 text-primary" >Digital Token for Everyone</p>
-                            <p className="card-text fs-14">Kubix is established by KASIKORN X, a company under KBTG, to operate as an Initial Coin Offering (ICO) portal. Broadening the horizon beyond....</p>
-                        </div>
-                    </div>
-                    <div className="cardshadow col-3 m-3" >
-                        <img src="/Bentagro.png" className="card-img-top" alt="..."/>
-                        <div className="card-body p-3">
-                            <p className="card-title fs-26">Betagro</p>
-                            <p className="card-subtitle fs-14 mb-2 text-primary" >Digital Token for Everyone</p>
-                            <p className="card-text fs-14">Kubix is established by KASIKORN X, a company under KBTG, to operate as an Initial Coin Offering (ICO) portal. Broadening the horizon beyond....</p>
-                        </div>
-                    </div>
-                    <div className="cardshadow col-3 m-3" >
-                        <img src="/Mercedes Benz.png" className="card-img-top" alt="..."/>
-                        <div className="card-body p-3">
-                            <p className="card-title fs-26">Mercedes Benz</p>
-                            <p className="card-subtitle fs-14 mb-2 text-primary" >Digital Token for Everyone</p>
-                            <p className="card-text fs-14">Kubix is established by KASIKORN X, a company under KBTG, to operate as an Initial Coin Offering (ICO) portal. Broadening the horizon beyond....</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="d-flex justify-content-center mt-5" >
-                    <button type="button" className="btn btn-primary text-light rounded-pill">All Project</button>
-                </div>
+  return (
+    <section id="projects" className="py-8 overflow-hidden">
+      <div className="container">
+        <h2 className="text-center fs-60 mb-6">
+          our <span className="text-primary">projects</span>
+        </h2>
+
+        <div className="row g-2 g-md-5">
+          <div className="col-6 col-md-4">
+            <div class="cardshadow">
+              <img src="/Sansiri.png" className="card-img-top" alt="..." />
+              <div className="card-body p-3">
+                <p className="card-title fs-26">Sansiri</p>
+                <p className="card-subtitle fs-14 mb-2 text-primary">
+                  Digital Token for Everyone
+                </p>
+                <p className="card-text fs-14">
+                  Kubix is established by KASIKORN X, a company under KBTG, to
+                  operate as an Initial Coin Offering (ICO) portal. Broadening
+                  the horizon beyond....
+                </p>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+          <div className="col-6 col-md-4">
+            <div class="cardshadow">
+              <img src="/Sansiri.png" className="card-img-top" alt="..." />
+              <div className="card-body p-3">
+                <p className="card-title fs-26">Sansiri</p>
+                <p className="card-subtitle fs-14 mb-2 text-primary">
+                  Digital Token for Everyone
+                </p>
+                <p className="card-text fs-14">
+                  Kubix is established by KASIKORN X, a company under KBTG, to
+                  operate as an Initial Coin Offering (ICO) portal. Broadening
+                  the horizon beyond....
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4">
+            <div class="cardshadow">
+              <img src="/Sansiri.png" className="card-img-top" alt="..." />
+              <div className="card-body p-3">
+                <p className="card-title fs-26">Sansiri</p>
+                <p className="card-subtitle fs-14 mb-2 text-primary">
+                  Digital Token for Everyone
+                </p>
+                <p className="card-text fs-14">
+                  Kubix is established by KASIKORN X, a company under KBTG, to
+                  operate as an Initial Coin Offering (ICO) portal. Broadening
+                  the horizon beyond....
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center mt-5">
+          <button
+            type="button"
+            className="btn btn-primary text-light rounded-pill px-4"
+          >
+            All Project
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
